2ee5001cb7768b1e19abbbe01931930605482a3f
Kevin Yu <kevin@Kevins-MacBook-Pro-2.local>
2020-10-11 19:45:19 -0400

    add functions for pulling most recent COVID manufacturing requests

---
+++ Code_CovidFabForm.js
@@ -0,0 +1,38 @@
+/*
+Document: JS code connecting the COVID Fab form to the spreadsheet
+*/
+
+var counter = getNextCovidRequestId();
+
+/**
+ * Pulls the most recent COVID manufacturing request and places it in the PM 21 spreadsheet.
+ *
+ * @return {void}
+ */
+function onCovidRequestSubmit() {
+  var form = FormApp.openById(scriptProps.getProperty('covidFabForm'));
+  var responses = form.getResponses();
+  var latestResponse = responses[responses.length - 1];
+  var responseData = latestResponse.getItemResponses();
+  responseData = responseData.map(ele => ele.getResponse()); // convert items to item responses
+  // var requestId = getNextChangeRequestId(responseData.splice(2, 1)[0]); // convert request type to id num
+  var requestId = counter(); // convert request type to id num
+  responseData.unshift(requestId, latestResponse.getTimestamp()); // add id num and timestamp to front of array
+  var sheet = getSheetInfo('mainSheetID', 'Fab & Weld', 'sheet');
+  sheet.appendRow(responseData);
+}
+
+/**
+ * Increments and returns the counter used for the next COVID manufacturing request ID.
+ *
+ * @returns {function(): number} The next COVID manufacturing request ID.
+ */
+function getNextCovidRequestId() {
+  var counter = 0;
+
+  function increment() {
+    counter++;
+    return counter;
+  }
+  return increment;
+}
