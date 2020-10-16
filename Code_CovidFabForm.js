851a8cc65110108cda6003f8972c819f02bb2010
Kevin Yu <kevin@Kevins-MacBook-Pro-2.local>
2020-10-15 21:56:18 -0400

    i17 resolved - simplify getNextCovidRequestId function

--- Code_CovidFabForm.js
+++ Code_CovidFabForm.js
@@ -20,7 +20,7 @@ function onCovidRequestSubmit() {
   var responseData = latestResponse.getItemResponses();
   responseData = responseData.map(ele => ele.getResponse()); // convert items to item responses
   // var requestId = getNextChangeRequestId(responseData.splice(2, 1)[0]); // convert request type to id num
-  var requestId = getNextCovidRequestId(); // convert request type to id num
+  var requestId = getNextCovidRequestId();
   responseData.unshift(requestId, latestResponse.getTimestamp()); // add id num and timestamp to front of array
   var sheet = getSheetInfo('mainSheetID', 'Fab & Weld', 'sheet');
   sheet.appendRow(responseData);
@@ -34,8 +34,7 @@ function onCovidRequestSubmit() {
 function getNextCovidRequestId() {
   const sheet = getSheetInfo('mainSheetID', 'Settings', 'sheet');
   let cell = sheet.getRange(7, 2);
-  let value = cell.getValues();
-  let id = value[0][0];
-  cell.setValue(++id);
-  return id;
+  let nextId = cell.getValue() + 1;
+  cell.setValue(nextId);
+  return nextId;
 }
