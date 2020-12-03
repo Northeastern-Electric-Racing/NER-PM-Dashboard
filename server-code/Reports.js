997fe461e5a66b0ef99815dc9fdef8a4269a971b
Kevin Yu <kevin@Kevins-MacBook-Pro-2.local>
2020-12-02 21:02:15 -0500

    i6 - add comments and clean up code

--- server-code/Reports.js
+++ server-code/Reports.js
@@ -9,7 +9,6 @@ function getReport(reportType) {
     if (reportType == "dashboard") {
         return getPlaceholderHTML("* Coming Soon! *");
     } else if (reportType == "deadlines") {
-        // return getPlaceholderHTML("* Coming Soon! *");
         return getUpcomingDeadlines();
     } else if (reportType == "changes") {
         return getAllChangeRequests();
@@ -28,38 +27,38 @@ function getAllChangeRequests() {
 }
 
 /**
+ * Returns an HTML formatted table of all active work packages that are due this week and next week.
  *
- * @returns {string}
+ * @returns {string} An HTML table with work packages due this week and next week.
  */
 function getUpcomingDeadlines() {
     let data = getSheetInfo('mainSheetID', 'Work Packages', 'data');
-
-    // iterate through data and get 3 json arrays
-        // construct main content (3 tables) given the 3 json arrays
     return buildDeadlinesTable(data);
 }
 
 /**
+ * Builds the HTML table with active work packages due this week and next week.
  *
- * @param data
- * @returns {string}
+ * @param {Object[][]} data - The content from the 'work packages' tab in the PM 21 spreadsheet.
+ * @returns {string} An HTML table with the work packages due this week and next week.
  */
 function buildDeadlinesTable(data) {
     let thisWeek = [];
     let nextWeek = [];
 
+    // add headers for tables
     thisWeek.push(["WBS #", "Project", "Name"]);
     nextWeek.push(["WBS #", "Project", "Name"]);
 
     for(let row = 1; row < data.length; row++) {
-
+        // check if work package is active or not
         if(data[row][4] === "A") {
+            let task = [data[row][2], data[row][0], data[row][3]];
+
             let date = new Date();
             let mondayOfCurrWeek = getMondayOfCurrWeek(date);
             let deadline = new Date(data[row][9]);
             let differenceInDays = (deadline - mondayOfCurrWeek) / 1000 / 60 / 60 / 24;
-
-            let task = [data[row][2], data[row][0], data[row][3]];
 
             if(differenceInDays >-1 && differenceInDays <= 6) {
                 thisWeek.push(task);
@@ -67,42 +66,12 @@ function buildDeadlinesTable(data) {
                 nextWeek.push(task);
             }
         }
-
-
     }
-
-    // thisWeek.map(task => {
-    //     html += `<div class="">
-    //                ` + task.wbs + task.project + task.name + `
-    //             </div>`
-    // });
-    // nextWeek.map(task => {
-    //     html += `<div class="">
-    //                ` + task + `
-    //             </div>`
-    // });
-    // thisWeek.map(task => {
-    //     html += `<div class="">
-    //                ` + task + `
-    //             </div>`
-    // });
-
-     // html += `<div class="">
-     //               ` + deadline + `
-     //            </div>`
-
-
-
-
-    // div class='content-container'
-    // delegate to helper 3 times:
-        // create tables with the js object arrays
-            // div class='row' for each table
+    // constructing the tables separately
     let thisWeekTable = buildTableHTML(thisWeek, "table-md");
     let nextWeekTable = buildTableHTML(nextWeek, "table-md");
 
-
-    // add/combine tables together and return
+    // return combined table
     return `<div class="upcoming-deadline-flex-container">
            <h3>This Week</h3>
             ` + thisWeekTable + `
@@ -112,9 +81,11 @@ function buildDeadlinesTable(data) {
 }
 
 /**
+ * Gets the beginning of the week by returning the date of the Monday of the provided date.
+ * For instance, if the date 12/3/20 is provided, then the function will return 11/30/20.
  *
- * @param date
- * @returns {Date}
+ * @param {Date} date - The date in which to get the beginning of the week for.
+ * @returns {Date} The beginning of the week (Monday) for the provided date.
  */
 function getMondayOfCurrWeek(date) {
     date = new Date(date);
