e0338d50369f522cb0a2fa9b97313e72e3f65a3f
GoodbyE <eshwaribhide@GoodbyE.attlocal.net>
2021-01-06 15:47:45 -0800

    Creating a wrapper for link function

--- server-code/Projects.js
+++ server-code/Projects.js
@@ -11,3 +11,14 @@ function getAllProjects() {
     var data = getSheetInfo('mainSheetID', 'Projects', 'data');
     return buildTableHTML(data, "table-sm");
 }
+
+/**
+ * Creates an HTML hyperlink, given the display text, that requests the given url.
+ * 
+ * @param {String} url – The url to link to
+ * @param {String} displayText – The text that will be displayed
+ * @return {String} – The corresponding HTML hyperlink
+ */
+function getHTMLLink(url, displayText) {
+    return displayText.link(url);
+}
