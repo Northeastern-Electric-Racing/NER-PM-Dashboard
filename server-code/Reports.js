f75985a312a2fecf36014bbedacd63501e74bcaf
RahulR100 <rahulrao@email.com>
2021-01-08 22:45:24 -0500

    Added fab and weld report generator

--- server-code/Reports.js
+++ server-code/Reports.js
@@ -12,7 +12,7 @@ function getReport(reportType) {
     if (reportType == "changes") {
         return getAllChangeRequests();
     } else if (reportType == "fab") {
-        return getPlaceholderHTML("* Coming Soon! *");
+        return getAllFabLogs();
     } else {
         throw "Requested report type not supported: " + reportType;
     }
@@ -25,5 +25,15 @@ function getReport(reportType) {
  */
 function getAllChangeRequests() {
     var data = getSheetInfo('mainSheetID', 'Change Requests', 'data');
+    return buildTableHTML(data, "table-sm");
+}
+
+/**
+* Returns HTML formatted list of all fab and weld reports
+*
+* @return {String} - Constructed HTML table listing all the fab and weld reports
+*/
+function getAllFabLogs() {
+	var data = getSheetInfo('mainSheetID', 'Fab & Weld', 'data');
     return buildTableHTML(data, "table-sm");
 }
