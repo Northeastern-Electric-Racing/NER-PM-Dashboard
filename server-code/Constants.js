0f687aeb9cc87aa20ae1e1143630450f209ffdba
Eshwari Bhide <ebhide99@gmail.com>
2021-01-08 18:05:56 -0800

    i85-creating constants file and moving some constants into it

---
+++ server-code/Constants.js
@@ -0,0 +1,9 @@
+/*
+Constants file to follow best practice of keeping things in one place.
+*/
+
+const MAIN_SHEET_ID_STR = "mainSheetID";
+
+
+const SLIDE_DECK_STR = "Slide Deck";
+const BOM_STR = "BOM";
