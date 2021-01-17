e5ba0fdbab40a7ce2be4bd764c530bf1439a6baa
Eshwari Bhide <ebhide99@gmail.com>
2021-01-16 20:20:03 -0800

    i103-removing headers

--- server-code/Reports.js
+++ server-code/Reports.js
@@ -59,8 +59,9 @@ function getReviewedChangeRequests() {
 function getOpenChangeRequests() {
     var data = getSheetInfo(MAIN_SHEET_ID_STR, CHANGE_REQUESTS_STR, DATA_STR);
     var headers = data[0];
-    var openChangeRequestsData = [headers];
     var reviewedColIdx = findIdx("Reviewed", headers);
+    headers = headers.slice(0, -5);
+    var openChangeRequestsData = [headers];
     for (var rowIdx = 1; rowIdx < data.length; rowIdx++) {
         if (!(data[rowIdx][reviewedColIdx])) {
             openChangeRequestsData.push(data[rowIdx].slice(0, -5));
