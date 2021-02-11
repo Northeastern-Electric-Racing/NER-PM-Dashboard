dbe27fa8d8ddc65ad72fe410f9362485d1acd8c5
Jeff <Airlegoland@gmail.com>
2021-02-11 13:50:36 -0500

    #172 - added tests to findIdx

--- server-code/Main/Main.test.ts
+++ server-code/Main/Main.test.ts
@@ -1,11 +1,17 @@
 import { findIdx } from "./Main";
 
-test("find index of array", () => {
+test("find index of number in array", () => {
   expect(findIdx(3, [5, 4, 3, 0, 3])).toBe(2);
 });
 
-test("find index of array", () => {
+test("find index of String in array", () => {
   expect(
     findIdx("Hello", ["first", "second", "hello ", "Hello", "fifth"])
   ).toBe(3);
+});
+
+test("find index of String not in array", () => {
+  expect(() =>
+    findIdx("hello", ["first", "second", "hello ", "Hello", "fifth"])
+  ).toThrow("Item not found in array");
 });
